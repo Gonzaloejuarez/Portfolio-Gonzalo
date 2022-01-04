ab015665ccde2312d131c5d0b5f2284ddb371265
Gonzalo Juarez <juarezgonzalo000@gmail.com>
2022-01-04 15:28:14 -0300

    instalacion de paquetes

---
+++ client/src/Components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Home = () => {
+    return(
+        <div>
+            <h1>
+                this is my portfolio in ReactJS
+            </h1>
+        </div>
+    )
+}
+export default Home
